7c785c6d9fc471fa2c914ccc82e7263407acc906
Dhina17 <dhinalogu@gmail.com>
2020-08-09 01:21:40 +0530

    drivers: usb: Import Xiaomi changes
    Change-Id: I004a28738e8fc5831aae3a54f6bcefdcff603f11
    Signed-off-by: Risti699 <wahhap10@gmail.com>

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -1,6 +1,15 @@
-// SPDX-License-Identifier: GPL-2.0-only
-/*
- * Copyright (c) 2012-2021, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2020, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2020 XiaoMi, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
  */
 
 #include <linux/module.h>
@@ -4639,6 +4648,7 @@ static int dwc3_msm_gadget_vbus_draw(struct dwc3_msm *mdwc, unsigned int mA)
 	union power_supply_propval pval = {0};
 	int ret, psy_type;
 
+	dev_info(mdwc->dev,"dwc3_msm_gadget_vbus_draw %u\n",mA);
 	psy_type = get_psy_type(mdwc);
 	if (psy_type == POWER_SUPPLY_TYPE_USB_FLOAT) {
 		/*
@@ -4656,9 +4666,13 @@ static int dwc3_msm_gadget_vbus_draw(struct dwc3_msm *mdwc, unsigned int mA)
 		goto set_prop;
 	}
 
-	if (mdwc->max_power == mA || psy_type != POWER_SUPPLY_TYPE_USB)
+	if (mdwc->max_power == mA || ((psy_type != POWER_SUPPLY_TYPE_USB) && (psy_type != POWER_SUPPLY_TYPE_USB_CDP)))
 		return 0;
 
+	if((psy_type == POWER_SUPPLY_TYPE_USB_CDP) && (mA > 100))
+		mA = 1500;
+
+	dev_info(mdwc->dev, "Avail curr from USB = %u\n", mA);
 	/* Set max current limit in uA */
 	pval.intval = 1000 * mA;
 
@@ -4760,6 +4774,17 @@ static void dwc3_otg_sm_work(struct work_struct *w)
 				atomic_read(&mdwc->dev->power.usage_count));
 			dwc3_otg_start_peripheral(mdwc, 1);
 			mdwc->drd_state = DRD_STATE_PERIPHERAL;
+
+            if(!dwc->softconnect && get_psy_type(mdwc) == POWER_SUPPLY_TYPE_USB_CDP) {
+                u32 reg;
+                dbg_event(0xFF, "cdp pullup dp", 0);
+
+                reg = dwc3_readl(dwc->regs, DWC3_DCTL);
+                reg |= DWC3_DCTL_RUN_STOP;
+                dwc3_writel(dwc->regs, DWC3_DCTL, reg);
+                break;
+            }
+
 			work = 1;
 		} else {
 			if (mdwc->usb_data_enabled)
